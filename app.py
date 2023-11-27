c7ecb3aacae003c20517e92b1ab0f3663d400eb1
RishiUpadhyay <71641273+RishiUpadhyay@users.noreply.github.com>
2023-11-27 17:07:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+# import the necessary modules
+from flask import Flask , render_template , request , jsonify
+
+# importing sentiment_analysis file as sa
+import sentiment_analysis as sa
+
+app = Flask(__name__)
+
+# app route for index page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# write a route for post request
+@app.route("/predict-emotion", methods=["POST"])
+def predict_emotion():
+    input_text = request.json.get("text")
+    if not input_text:
+        return jsonify({
+            "status": "error",
+            "message": "Please enter some text to predict emotion!"
+        }), 400
+    else:
+        predicted_emotion, predicted_emotion_img_url = predict(input_text)                         
+        return jsonify({
+            "data": {
+                "predicted_emotion": predicted_emotion,
+                "predicted_emotion_img_url": predicted_emotion_img_url
+            },
+            "status": "success"
+        }), 200
+
+if __name__  ==  "__main__":
+    app.run(debug = True)
